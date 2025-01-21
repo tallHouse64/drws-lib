856b8d6d9c09031b29a4847203d95d4a1738082f
tallHouse64 <tallhouse64@protonmail.com>
2025-01-21 21:08:56 +0000

    Partly tested D_PLATFORM_IMPLEMENTATION and D_PLATFORM_ALREADY_IMPLEMENTED.

---
+++ platform/test/includetest/includetest.c
@@ -0,0 +1,31 @@
+#include"../../sdld.h"
+#include"../../sdld.h"
+#include"../../sdld.h"
+#include"../../sdld.h"
+#define D_PLATFORM_IMPLEMENTATION
+#include"../../sdld.h"
+#include"../../sdld.h"
+#include"../../sdld.h"
+
+#define D_IMPLEMENTATION
+#include"../../../d.h"
+
+#define DEVENTS_IMPLEMENTATION
+#include"../../../devents.h"
+
+#include<stdio.h>
+
+int main(){
+
+    D_Surf * out = D_GetOutSurf(50, 50, 640, 480, "Include test for platform");
+
+    D_FillRect(out, D_NULL, D_rgbaToFormat(out->format, 255, 170, 120, 255));
+    D_FlipOutSurf(out);
+
+    D_Delay(3000);
+
+    D_FreeOutSurf(out);
+    out = D_NULL;
+
+    printf("Works.\n");
+};
