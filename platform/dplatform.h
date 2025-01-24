69bb9df6c66da879ba8882a1512411962564a8cb
tallHouse64 <tallhouse64@protonmail.com>
2025-01-24 21:20:04 +0000

    Added D_OutSurfFlags enum to dplatform.h, not used yet.

--- platform/dplatform.h
+++ platform/dplatform.h
@@ -3,6 +3,10 @@
 #ifndef DPLATFORM_H
 #define DPLATFORM_H
 
+typedef enum D_OutSurfFlags {
+    D_OUTSURFRESIZABLE = 0x01
+} D_OutSurfFlags;
+
 D_Surf * D_GetOutSurf(int x, int y, int w, int h, char * title);
 int D_FreeOutSurf(D_Surf * s);
 int D_FlipOutSurf(D_Surf * s);
