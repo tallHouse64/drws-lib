db45ea18e2b99e8e9059f40132f169f8b00cc7b0
tallHouse64 <tallhouse64@protonmail.com>
2025-02-10 12:27:59 +0000

    Tested D_LoadImage() with D_ALLOW_STB_IMAGE defined, working.

--- test/loadtest.c
+++ test/loadtest.c
@@ -1,42 +1,35 @@
 #include<SDL2/SDL.h>
 
-#define ALLOW_STB_IMAGE
-#include"d.h"
+#define D_IMPLEMENTATION
+#define D_ALLOW_STB_IMAGE
+#include"../d.h"
+
+#define DEVENTS_IMPLEMENTATION
+#include"../devents.h"
+
+#define D_PLATFORM_IMPLEMENTATION
+#include"../platform/sdld.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include"stb_image.h"
+#include"../stb_image.h"
 
-int showSurf(D_Surf * s){
-
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window * w = SDL_CreateWindow("test", 10, 10, s->w/8, s->h/8, 0);
-    SDL_Surface * ws = SDL_GetWindowSurface(w);
-    SDL_Surface * sdls = SDL_CreateRGBSurfaceFrom(s->pix, s->w, s->h, 32, s->w * 4, 0xFF, 0xFF00, 0xFF0000, 0xFF000000);
-
-    SDL_FillRect(ws, NULL, 0);
-    SDL_BlitScaled(sdls, NULL, ws, NULL);
-
-
-    SDL_UpdateWindowSurface(w);
-
-    SDL_Delay(3000);
-
-    SDL_DestroyWindow(w);
-    ws = NULL;
-    w = NULL;
-    SDL_FreeSurface(sdls);
-    sdls = NULL;
-
-    return 0;
-};
+D_Surf * out = D_NULL;
+D_Surf * img = D_NULL;
 
 int main(){
-    D_Surf * s = D_LoadImage("folder.png");
+    img = D_LoadImage("img/folder.png");
+    printf("Loaded\n");
 
-    showSurf(s);
+    out = D_GetOutSurf(50, 50, 640, 480, "Testing D_LoadImage()", 0);
 
-    D_FreeSurf(s);
+    D_SurfCopyScale(img, D_NULL, out, D_NULL);
+
+    D_FlipOutSurf(out);
+
+    D_Delay(3000);
+
+    D_FreeSurf(img);
+    img = D_NULL;
 
     return 0;
 };
