8f4f446a2078d3433441ac8238d2c69e9b14f1e3
tallHouse64 <tallhouse64@protonmail.com>
2025-08-15 17:16:41 +0100

    Added c89 validation test for d.h called 'stdc89testdh.c'.

---
+++ test/stdc89testdh.c
@@ -0,0 +1,11 @@
+#include"../d.h"
+
+/* The purpose of this test is to find where the
+ *  d.h file does not meet the C89 standard.
+ *
+ * Run this test with "gcc stdc89testdh.c ../d.h --std=c89".
+ */
+
+int main(int argc, char ** argv){
+    return 0;
+};
