4400405fec50a94e88c3ff1888a68a7ec2849dbd
tallHouse64 <tallhouse64@protonmail.com>
2025-08-30 21:06:27 +0100

    Added autotest.c and changed chartomaptest.c to run in it.

---
+++ test/autotest.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+#define D_IMPLEMENTATION
+#include"../d.h"
+
+int dontPrintf(char * format, ...){
+    return 0;
+};
+
+/* This is is an automatic test.
+ */
+
+
+/* Levels of verbosity change to turn of a level
+ *  define it as dontPrintf, to turn it on,
+ *  define it as printf.
+ *
+ * V0 is for printing the details of each subtest
+ *  and whether it was successful.
+ * V1 is for printing weather each test (and it's
+ *  subtests) succeeded.
+ * V2 is for printing weather all tests were
+ *  successful.
+ */
+#define TEST_PRINTF_V0 printf
+#define TEST_PRINTF_V1 printf
+#define TEST_PRINTF_V2 printf
+
+
+/*Include the .c files directly. */
+#include"chartomaptest.c"
+
+int main(int argc, char ** argv){
+
+    int failedTests = 0;
+
+    failedTests += D_CharToMapTest();
+
+    TEST_PRINTF_V2("All tests overall ");
+    if(failedTests == 0){
+        TEST_PRINTF_V2("Success\n\n");
+        return 0;
+    }else{
+        TEST_PRINTF_V2("Failed (%d tests failed)\n\n", failedTests);
+        return 1;
+    };
+
+    return 0;
+};
--- test/chartomaptest.c
+++ test/chartomaptest.c
@@ -1,53 +1,91 @@
-#include<stdio.h>
-
-#define D_IMPLEMENTATION
-#include"../d.h"
 
 /* This tests D_CharToMap() in d.h. It does not
  *  test every character but if some work they
  *  should all work, especially the first and
  *  last.
+ *
+ * This test is part of autotest.c.
  */
 
-int test(char c, int eX, int eY){
-    int x = -1;
-    int y = -1;
+int D_CharToMapSubTest(char c, int eX, int eY, int eRet){
+    int x = -2;
+    int y = -2;
 
     int ret = D_CharToMap(c, &x, &y);
 
-    printf("Char %c,   x: %d y: %d\nShould be x: %d y: %d\n", c, x, y, eX, eY);
+    TEST_PRINTF_V0("Char %c,   x: %d y: %d\nShould be x: %d y: %d\n", c, x, y, eX, eY);
 
-    printf("returned %d\n\n", ret);
+    TEST_PRINTF_V0("returned %d\n", ret);
 
-    return 0;
+    if(x == eX && y == eY && ret == eRet){
+        TEST_PRINTF_V0("Success\n\n");
+        return 0;
+    }else{
+        TEST_PRINTF_V0("Failed\n\n");
+        return 1;
+    };
+
+    return 1;
 };
 
-int main(){
+int D_CharToMapTest(){
 
-    printf("Test capital letters\n");
-    test('A', 0, 0);
-    test('B', 1, 0);
-    test('F', 5, 0);
-    test('Z', 7, 2);
+    int failedTests = 0;
 
-    printf("\nTest lower case letters\n");
-    test('a', 0, 3);
-    test('b', 1, 3);
-    test('n', 4, 4);
-    test('u', 2, 5);
-    test('z', 7, 5);
+    TEST_PRINTF_V0("Test capital letters\n");
+    failedTests += D_CharToMapSubTest('A', 0, 0, 0);
+    failedTests += D_CharToMapSubTest('B', 1, 0, 0);
+    failedTests += D_CharToMapSubTest('F', 5, 0, 0);
+    failedTests += D_CharToMapSubTest('Z', 7, 2, 0);
 
-    printf("\nTest numbers\n");
-    test('0', 0, 6);
-    test('1', 1, 6);
-    test('2', 2, 6);
-    test('7', 7, 6);
-    test('9', 0, 7);
+    TEST_PRINTF_V0("\nTest lower case letters\n");
+    failedTests += D_CharToMapSubTest('a', 0, 3, 0);
+    failedTests += D_CharToMapSubTest('b', 1, 3, 0);
+    failedTests += D_CharToMapSubTest('n', 4, 4, 0);
+    failedTests += D_CharToMapSubTest('u', 2, 5, 0);
+    failedTests += D_CharToMapSubTest('z', 7, 5, 0);
 
-    printf("Test some symbols\n");
-    test('.', 1, 7);
-    test(',', 2, 7);
-    test('!', 8, 7);
-    test('"', 0, 8);
-    test('@', 7, 8);
+    TEST_PRINTF_V0("\nTest numbers\n");
+    failedTests += D_CharToMapSubTest('0', 0, 6, 0);
+    failedTests += D_CharToMapSubTest('1', 1, 6, 0);
+    failedTests += D_CharToMapSubTest('2', 2, 6, 0);
+    failedTests += D_CharToMapSubTest('7', 7, 6, 0);
+    failedTests += D_CharToMapSubTest('9', 0, 7, 0);
+
+    TEST_PRINTF_V0("\nTest some symbols\n");
+    failedTests += D_CharToMapSubTest('.', 1, 7, 0);
+    failedTests += D_CharToMapSubTest(',', 2, 7, 0);
+    failedTests += D_CharToMapSubTest('!', 8, 7, 0);
+    failedTests += D_CharToMapSubTest('"', 0, 8, 0);
+    failedTests += D_CharToMapSubTest('@', 7, 8, 0);
+
+    TEST_PRINTF_V0("\nTest some more symbols\n");
+    failedTests += D_CharToMapSubTest('^', 0, 9, 0);
+    failedTests += D_CharToMapSubTest('*', 1, 9, 0);
+    failedTests += D_CharToMapSubTest('-', 2, 9, 0);
+    failedTests += D_CharToMapSubTest('_', 3, 9, 0);
+    failedTests += D_CharToMapSubTest('+', 4, 9, 0);
+    failedTests += D_CharToMapSubTest('=', 5, 9, 0);
+    failedTests += D_CharToMapSubTest('|', 6, 9, 0);
+    failedTests += D_CharToMapSubTest('\\', 7, 9, 0);
+    failedTests += D_CharToMapSubTest('{', 8, 9, 0);
+
+    failedTests += D_CharToMapSubTest('}', 0, 10, 0);
+    failedTests += D_CharToMapSubTest('[', 1, 10, 0);
+    failedTests += D_CharToMapSubTest(']', 2, 10, 0);
+    failedTests += D_CharToMapSubTest('<', 3, 10, 0);
+    failedTests += D_CharToMapSubTest('>', 4, 10, 0);
+    failedTests += D_CharToMapSubTest('~', 5, 10, 0);
+    failedTests += D_CharToMapSubTest('`', 6, 10, 0);
+
+    TEST_PRINTF_V1("\nD_CharToMapTest overall ");
+    if(failedTests == 0){
+        TEST_PRINTF_V1("Success\n\n");
+        return 0;
+    }else{
+        TEST_PRINTF_V1("Failed (%d subtests failed)\n\n", failedTests);
+        return 1;
+    };
+
+    return 1;
 };
